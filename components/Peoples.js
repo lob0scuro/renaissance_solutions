ac3120fa379bf4d2dadd2c6c53eb39d996e2fcda
lob0scuro <kamrin717@gmail.com>
2024-03-29 02:18:49 -0500

    added about section with intake form

--- components/Peoples.js
+++ components/Peoples.js
@@ -1,18 +1,18 @@
-app.component('peoples', {
-    props: {
-        persons: {
-            type: Array,
-            required: true,
-        }
+app.component("peoples", {
+  props: {
+    persons: {
+      type: Array,
+      required: true,
     },
-    template:
+  },
+  template:
     /*html*/
-`   <div v-for="(person, index) in persons" class="personBlock" id="index">
+    `   <div v-for="(person, index) in persons" class="personBlock" id="index">
         <div  class="personBlurb" >
             <h5>{{person.first_name}} {{person.last_name}} - <small>{{person.title}}</small></h5>
         
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia omnis tempora esse quisquam porro laboriosam voluptas architecto. Error, reprehenderit iure!</p>
         </div>
-        <img src="./images/employee.jpg" alt="employee photo" width="60%" height="auto">
-    </div>`
-})
+        <img src="./images/employee.jpg" alt="employee photo" width="50%" height="auto">
+    </div>`,
+});
