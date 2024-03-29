957a43c1f1542dc616f328e3cc9fb16014994297
lob0scuro <kamrin717@gmail.com>
2024-03-28 23:51:04 -0500

    started persons section

--- main.js
+++ main.js
@@ -25,41 +25,77 @@ const app = Vue.createApp({
         {
           first_name: "Jimmy",
           last_name: "Trahan",
-          title: "Professional Counselor",
+          title: "LPC",
           role: "Admin",
         },
         {
-          first_name: "Oshe",
-          last_name: "Lewis-Reese",
-          title: "Professional Counselor",
+          first_name: "Christina",
+          last_name: "LeBlanc",
+          title: "Office Manager",
+          role: "Admin",
+        },
+        {
+          first_name: "Sarah",
+          last_name: "Walker",
+          title: "APRN-PMHNP",
+          role: "Admin",
+        },
+        {
+          first_name: "Dominique",
+          last_name: "Rougeau",
+          title: "ABA",
+          role: "Admin",
+        },
+        {
+          first_name: "Jaqueline",
+          last_name: "Taylor",
+          title: "APRN-PMHNP",
+          role: "Employee",
+        },
+        {
+          first_name: "Katherine",
+          last_name: "Devillier",
+          title: "APRN-PMHNP",
           role: "Employee",
         },
         {
           first_name: "Lindsay",
           last_name: "Lopez",
-          title: "Professional Counselor",
+          title: "LPC",
           role: "Employee",
         },
         {
-          first_name: "Sarah",
-          last_name: "Walker",
-          title: "Nurse Practitioner",
+          first_name: "Kehri",
+          last_name: "Jones",
+          title: "BCBA",
           role: "Employee",
         },
         {
-          first_name: "Jaqueline",
-          last_name: "Taylor",
-          title: "Nurse Practitioner",
+          first_name: "Jirita",
+          last_name: "Atkins",
+          title: "BCBA",
+          role: "Employee",
+        },
+        {
+          first_name: "Todd",
+          last_name: "Furman",
+          title: "BCBA",
+          role: "Employee",
+        },
+        {
+          first_name: "Todd",
+          last_name: "Gatte",
+          title: "APRN-PMHNP",
           role: "Employee",
         },
       ],
     };
   },
-  computed: {
-    fullName() {
-      for (person in this.persons) {
-        return person.first_name + " " + person.last_name;
-      }
+  methods: {
+    fullName(i) {
+      const person = this.persons[i];
+      // return person.first_name + " " + person.last_name;
+      return `${person.first_name} ${person.last_name}`;
     },
   },
 });
