bccd296e002927ae13d70cb6a2db452ea91a180c
lob0scuro <kamrin717@gmail.com>
2024-04-04 17:12:32 -0500

    added services section tabs

--- components/ServiceTabs.js
+++ components/ServiceTabs.js
@@ -1,5 +1,46 @@
-app.component("service-tabs", {
+app2.component("service-tabs", {
   template:
     /*html*/
-    ``,
+    `
+    <div class="tabs">
+          <input
+            type="radio"
+            id="psychotherapy"
+            name="serviceTabs"
+            checked="checked"
+          />
+          <label for="psychotherapy">Psychotherapy</label>
+          <div class="tab">
+            <h2>Psychotherapy</h2>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+              nostrum voluptas atque modi provident deserunt doloremque. Aperiam
+              ipsum, ex numquam unde, qui veniam itaque tempore recusandae,
+              nihil laborum repudiandae. Laudantium odio nisi ex cumque quod.
+            </p>
+          </div>
+          <input type="radio" id="psychmedmngmt" name="serviceTabs" />
+          <label for="psychmedmngmt">Psych. Med Management</label>
+          <div class="tab">
+            <h2>Psychiatric Medication Management</h2>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+              nostrum voluptas atque modi provident deserunt doloremque. Aperiam
+              ipsum, ex numquam unde, qui veniam itaque tempore recusandae,
+              nihil laborum repudiandae. Laudantium odio nisi ex cumque quod.
+            </p>
+          </div>
+          <input type="radio" id="aba" name="serviceTabs" />
+          <label for="aba">ABA</label>
+          <div class="tab">
+            <h2>Applied Behavioral Analysis</h2>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+              nostrum voluptas atque modi provident deserunt doloremque. Aperiam
+              ipsum, ex numquam unde, qui veniam itaque tempore recusandae,
+              nihil laborum repudiandae. Laudantium odio nisi ex cumque quod.
+            </p>
+          </div>
+        </div>
+    `,
 });
--- main.js
+++ main.js
@@ -1,1 +1,2 @@
 const app = Vue.createApp({});
+const app2 = Vue.createApp({})
