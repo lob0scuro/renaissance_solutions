8376ccad34babcff7c4b636dcafb21108c562981
lob0scuro <kamrin717@gmail.com>
2024-05-03 21:33:36 -0500

    started new barebones site

---
+++ main.js
@@ -0,0 +1,68 @@
+const persons = [
+  {
+    first_name: "Jimmy",
+    last_name: "Trahan",
+    title: "LPC",
+    role: "Admin",
+  },
+  {
+    first_name: "Christina",
+    last_name: "LeBlanc",
+    title: "Office Manager",
+    role: "Admin",
+  },
+  {
+    first_name: "Sarah",
+    last_name: "Walker",
+    title: "APRN-PMHNP",
+    role: "Admin",
+  },
+  {
+    first_name: "Dominique",
+    last_name: "Rougeau",
+    title: "ABA",
+    role: "Admin",
+  },
+  {
+    first_name: "Jaqueline",
+    last_name: "Taylor",
+    title: "APRN-PMHNP",
+    role: "Employee",
+  },
+  {
+    first_name: "Katherine",
+    last_name: "Devillier",
+    title: "APRN-PMHNP",
+    role: "Employee",
+  },
+  {
+    first_name: "Lindsay",
+    last_name: "Lopez",
+    title: "LPC",
+    role: "Employee",
+  },
+  {
+    first_name: "Kehri",
+    last_name: "Jones",
+    title: "BCBA",
+    role: "Employee",
+  },
+  {
+    first_name: "Jirita",
+    last_name: "Atkins",
+    title: "BCBA",
+    role: "Employee",
+  },
+  {
+    first_name: "Todd",
+    last_name: "Furman",
+    title: "BCBA",
+    role: "Employee",
+  },
+  {
+    first_name: "Todd",
+    last_name: "Gatte",
+    title: "APRN-PMHNP",
+    role: "Employee",
+  },
+];
